ea30ae06981557134909366213d7f327cb0060f6
Connor Arnison <connorarnison@gmail.com>
2019-07-30 10:48:36 +1000

    CPU Frequency and Temperature Measurement and Transmission

--- sc_client.py
+++ sc_client.py
@@ -6,41 +6,41 @@ import pickle
 
 
 class HWData:
-    def __init__(self, ip, time, cpu, memory):
-        self.ip = ip
+    def __init__(self, hostname, time, cpufreq, cpupercent, memory, temps):
+        self.hostname = hostname
         self.timestamp = time
-        self.cpu = cpu
+        self.cpufreq = cpufreq
+        self.cpupercent = cpupercent
         self.memory = memory
+        self.temps = temps
 
 
 def MeasureData():
-    ip = socket.gethostname()
+    hostname = socket.gethostname()
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    cpu = psutil.cpu_percent(interval=1)
-    memory = psutil.virtual_memory()[2]
-    return [ip, st, cpu, memory]
+    cpufreq = psutil.cpu_freq()
+    cpupercent = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+    temps = psutil.sensors_temperatures()
+    return [hostname, st, cpufreq, cpupercent, memory, temps]
 
 
-def EncodeData(ip, time, cpu, memory):
-    dataObject = HWData(ip, time, cpu, memory)
+def EncodeData(hostname, time, cpufreq, cpupercent, memory, temps):
+    dataObject = HWData(hostname, time, cpufreq, cpupercent, memory, temps)
     dataPickle = pickle.dumps(dataObject)
     return dataPickle
 
 
 host = socket.gethostname()
 port = 2004
-BUFFER_SIZE = 2048
 
 tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpClient.connect((host, port))
 
 
-
 while True:
     current_reading = MeasureData()
-    current_reading = EncodeData(current_reading[0], current_reading[1], current_reading[2], current_reading[3])
-    tcpClient.send(current_reading)
+    current_reading = EncodeData(current_reading[0], current_reading[1], current_reading[2], current_reading[3], current_reading[4], current_reading[5])
+    tcpClient.sendall(current_reading)
     time.sleep(2)
-
-tcpClient.close()
--- sc_server.py
+++ sc_server.py
@@ -3,12 +3,16 @@ from threading import Thread
 from socketserver import ThreadingMixIn
 import  pickle
 
+
 class HWData:
-    def __init__(self, ip, time, cpu, memory):
-        self.ip = ip
+    def __init__(self, hostname, time, cpufreq, cpupercent, memory, temps):
+        self.hostname = hostname
         self.timestamp = time
-        self.cpu = cpu
+        self.cpufreq = cpufreq
+        self.cpupercent = cpupercent
         self.memory = memory
+        self.temps = temps
+
 
 class ClientThread(Thread):
 
@@ -22,16 +26,11 @@ class ClientThread(Thread):
         while True:
             data = conn.recv(2048)
             data = pickle.loads(data)
-            if type(data) is HWData:
-                print(data.ip)
-                print(data.timestamp)
-                print(data.cpu)
-                print(data.memory)
-            #Handle Data here
+                # Handle Data here
+
 
 TCP_IP = '192.168.1.14'
 TCP_PORT = 2004
-BUFFER_SIZE = 2048
 
 tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
